5e4f78e508d6870d7822ebbe72e4012373848339
sylwestertylec <61873485+sylwestertylec@users.noreply.github.com>
2022-12-07 18:35:47 +0100

    Add widget with + and RST

--- lib/main.dart
+++ lib/main.dart
@@ -55,8 +55,10 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
-            Text('+'),
-            Text('RST'),
+            Row(children: <Widget>[
+              Text('+'),
+              Text('RST'),
+            ]),
           ],
         ),
       ),
